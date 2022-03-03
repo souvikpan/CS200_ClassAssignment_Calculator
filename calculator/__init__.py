948e0df037d09e422b243de54739330ea16bb7c1
Tishang Prajapati <tishangprajapati@iitbhilai.ac.in>
2022-03-03 14:48:10 +0530

    Binary exponentiation for calculation of exponential in O(log(n))

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -17,7 +17,27 @@ def ADD():
     sum=a+b
     response = "sum of 2 numbers = " + str(sum)
     return response
+
+@app.route("/expo", methods=["POST"])
+def expo(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
     
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    x=a
+    y=b
+ 
+    ans=1;
+    while(b):
+   
+        if(b&1): ans = (ans*a)
+        a = (a*a)
+        b=b>>1
+    
+    response = str(x) + "to the power " +str(y)+ " equals " +str(ans) 
+    return response 
+
 @app.route("/xyz", methods=["POST"])
 def XYZ(): 
     jsonStr = request.get_json()
@@ -29,15 +49,6 @@ def XYZ():
     # Logic for function assigned to you as in pdf
     
     return 1
-def subtract(a,b):
-    jsonStr = request.get_json()
-    jsonObj = json.loads(jsonStr)
-    a=int(jsonObj['N1'])
-    b=int(jsonObj['N2'])
-    return a-b
-    
-    
-    
 
 if __name__== "__main__":
     app.run()
