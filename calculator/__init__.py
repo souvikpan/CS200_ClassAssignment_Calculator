2ef88df078832ae02e2ac071e6202a29ade3c334
souvik <souvikp@iitbhilai.ac.in>
2022-03-03 15:02:31 +0530

    1st commit

---
+++ calculator/__init__.py
@@ -0,0 +1,34 @@
+import json
+from flask import Flask, render_template, request, jsonify   
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("InputOutput.html")        
+
+@app.route("/add", methods=["POST"])
+def ADD(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    sum=a+b
+    response = "sum = " + str(sum)
+    return response
+    
+@app.route("/xyz", methods=["POST"])
+def XYZ(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    
+    # Logic for function assigned to you
+    
+    return 1
+
+if __name__== "__main__":
+    app.run()
