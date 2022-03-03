59d36ecba84a65278e8ea884ac531611e5778df5
TarunSinghIIT <singh.tarun.iit@gmail.com>
2022-03-03 15:55:31 +0530

    grp_name_coders added a new function for lcm

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, render_template, request, jsonify   
+from math import lcm
 
 app = Flask(__name__)
 
@@ -38,7 +39,13 @@ def subtract(a,b):
     b=int(jsonObj['N2'])
     return a-b
     
-    
+@app.route("/lcm", methods=["POST"])
+def subtract(a,b):
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    return lcm(a,b)
     
 
 if __name__== "__main__":
