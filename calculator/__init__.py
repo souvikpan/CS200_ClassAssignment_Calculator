191bcfaaac1a7853d6255635057af6691f3e8d80
Prathamesh Gujar <prathameshgujar@iitbhilai.ac.in>
2022-03-04 15:11:45 +0530

    Added Logical And Function

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -32,6 +32,16 @@ def XYZ():
 
 
 
+@app.route("/LOGICAL_AND", methods=["POST"])
+def LOGICAL_AND(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    LOGIC_AND = a and b
+    response = "logical and of a and b is = " + str(LOGIC_AND)
+    return response
 
 if __name__== "__main__":
     app.run()
