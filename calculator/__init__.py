acba82f55b3bb19890086c064cc8491c19d0e249
Sudhir878786 <92601949+Sudhir878786@users.noreply.github.com>
2022-03-04 16:07:48 +0530

    done mod gfg cherrypick

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -30,7 +30,67 @@ def MOD():
     return response
 
     
+<<<<<<< HEAD
     # Logic for function assigned to you as in pdf
+=======
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+
+    log=math.log(a,b)
+    response="Log of Number-1 is calculated with  Number-2 as the base "+str(log)
+    return response
+@app.route("/multiply", methods=["POST"])
+def multiply(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    mul=a*b
+    response = f"multiplication of {a} and {b} = {mul}"
+    return response
+
+@app.route("/LOGICAL_AND", methods=["POST"])
+def LOGICAL_AND(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    LOGIC_AND = a and b
+    response = "logical and of a and b is = " + str(LOGIC_AND)
+
+    return response
+       
+@app.route("/shiftdeciright", methods=["POST"])
+def shiftdeciright(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=float(jsonObj['N1'])
+    b=float(jsonObj['N2'])
+    shift =a*10
+    shift2 =b*10
+    response ="Shfifting decimal right " + str(shift) +" and "+ str(shift2)
+    return response
+    
+
+@app.route("/lor", methods=["POST"])
+def lor(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+
+    a=jsonObj['N1']
+    b=jsonObj['N2']
+    num=a or b
+    response = "Logical or of 2 numbers is " + str(num)
+    return response
+    
+@app.route("/rightshift", methods=["POST"])
+def RIGHTSHIFT(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+>>>>>>> 9e08e9d... solved an error of other
     
     return 1
 
