90c6ce2c81675eae70e7202d659b98624d431780
Riya <78996450+riya107@users.noreply.github.com>
2022-03-04 15:20:02 +0530

    multiplication functionality added by group Tech Warriors

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -45,15 +45,26 @@ def bitAND():
     response = "And operator of 2 numbers is " + str(num)
     return response
 
+<<<<<<< HEAD
 @app.route("/logarithm", methods=["POST"])
 def logarithm(): 
+=======
+#code added by group tech warriors
+@app.route("/multiply", methods=["POST"])
+def multiply(): 
+>>>>>>> 218a054... multiplication functionality added by group Tech Warriors
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr)
     
     a=int(jsonObj['N1'])
     b=int(jsonObj['N2'])
+<<<<<<< HEAD
     log=math.log(a,b)
     response="Log of Number-1 is calculated with  Number-2 as the base "+str(log)
+=======
+    mul=a*b
+    response = f"multiplication of {a} and {b} = {mul}"
+>>>>>>> 218a054... multiplication functionality added by group Tech Warriors
     return response
 
 if __name__== "__main__":
