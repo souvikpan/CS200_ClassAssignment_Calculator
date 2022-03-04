ea457386f2a7bd750c0ca48fdcb7c89622ac403d
Nancy <nancygupta@iitbhilai.ac.in>
2022-03-04 20:48:22 +0530

    Expoential Function added

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,12 +1,10 @@
 import json
 from flask import Flask, render_template, request, jsonify   
-
 app = Flask(__name__)
-
+import math 
 @app.route("/")
 def home():
     return render_template("InputOutput.html")        
-
 @app.route("/add", methods=["POST"])
 def ADD(): 
     jsonStr = request.get_json()
@@ -16,20 +14,18 @@ def ADD():
     b=int(jsonObj['N2'])
     sum=a+b
     response = "sum of 2 numbers = " + str(sum)
-    return response
-    
-@app.route("/xyz", methods=["POST"])
-def XYZ(): 
+    return response    
+@app.route("/exponentiation", methods=["POST"])
+def exp(): 
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr)
     
-    a=int(jsonObj['N1'])
-    b=int(jsonObj['N2'])
-    
+    x=int(jsonObj['N1'])
+    y=int(jsonObj['N2'])
+    value=pow(x,y)
+    response = str(x) +" power " + str(y) + " is " + str(value)
+    return response
     # Logic for function assigned to you as in pdf
-    
-    return 1
-
 
 
 
