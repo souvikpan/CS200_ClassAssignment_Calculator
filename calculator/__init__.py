02bcc7933cec7a41479b24888fbcbe1ab6b91ec9
vanisha10 <agrawalvanisha10@gmail.com>
2022-03-04 15:21:41 +0530

    multiply

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -18,17 +18,18 @@ def ADD():
     response = "sum of 2 numbers = " + str(sum)
     return response
     
-@app.route("/xyz", methods=["POST"])
-def XYZ(): 
+@app.route("/multiply", methods=["POST"])
+def MULTIPLY(): 
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr)
     
     a=int(jsonObj['N1'])
     b=int(jsonObj['N2'])
-    
+    mult=a*b
+    response= "Product of 2 numbers = " + str(mult)
     # Logic for function assigned to you as in pdf
     
-    return 1
+    return response
 
 @app.route("/bitAND", methods=["POST"])
 def bitAND(): 
