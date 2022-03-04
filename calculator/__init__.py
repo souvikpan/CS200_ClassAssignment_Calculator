2312d7233b0b122d9d285cdd1d4b50c1ce40b725
Prathamesh Gujar <prathameshgujar@iitbhilai.ac.in>
2022-03-04 15:11:45 +0530

    BASH HUNTERS: Cherry picked Octal Brains branch commit

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -39,6 +39,16 @@ def bitAND():
     b=int(jsonObj['N2'])
     num=a&b
     response = "And operator of 2 numbers is " + str(num)
+
+@app.route("/LOGICAL_AND", methods=["POST"])
+def LOGICAL_AND(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    LOGIC_AND = a and b
+    response = "logical and of a and b is = " + str(LOGIC_AND)
     return response
 
 if __name__== "__main__":
