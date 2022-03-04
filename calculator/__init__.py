1d9629b53926159145fd9821cb60c72b7b5dfd3e
Abhinav Anand <anandabhinav220@gmail.com>
2022-03-04 15:57:38 +0530

    added hcf by TFM3

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,153 +1,175 @@
 import json
-from flask import Flask, render_template, request, jsonify 
-import math  
+from flask import Flask, render_template, request, jsonify
+import math
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return render_template("InputOutput.html")        
+    return render_template("InputOutput.html")
+
 
 @app.route("/add", methods=["POST"])
-def ADD(): 
+def ADD():
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr)
-    
-    a=int(jsonObj['N1'])
-    b=int(jsonObj['N2'])
-    sum=a+b
+
+    a = int(jsonObj['N1'])
+    b = int(jsonObj['N2'])
+    sum = a+b
     response = "sum of 2 numbers = " + str(sum)
     return response
-    
+
+
 @app.route("/expo", methods=["POST"])
-def expo(): 
-	jsonStr = request.get_json()
-	jsonObj = json.loads(jsonStr)
-	a=int(jsonObj['N1'])
-	b=int(jsonObj['N2'])
-	return str(a) + " raised to the power " + str(b)+ "is equal to "+ str(a**b)  
+def expo():
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    a = int(jsonObj['N1'])
+    b = int(jsonObj['N2'])
+    return str(a) + " raised to the power " + str(b) + "is equal to " + str(a**b)
+
 
 @app.route("/bitwiseXor", methods=["POST"])
-def bitwiseXor():     
-	jsonStr = request.get_json()
-	jsonObj = json.loads(jsonStr)
-	a=int(jsonObj['N1'])
-	b=int(jsonObj['N2'])
-	xor=a^b
-	response = "Bitwise Xor of 2 numbers = " + str(xor)
-	return response
+def bitwiseXor():
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    a = int(jsonObj['N1'])
+    b = int(jsonObj['N2'])
+    xor = a ^ b
+    response = "Bitwise Xor of 2 numbers = " + str(xor)
+    return response
+
 
 @app.route("/isequal", methods=["POST"])
 def isequal():
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr)
-    
-    a=int(jsonObj['N1'])
-    b=int(jsonObj['N2'])
-    
-    if (a==b):
+
+    a = int(jsonObj['N1'])
+    b = int(jsonObj['N2'])
+
+    if (a == b):
         return "The numbers are equal"
     else:
         return "The numbers are NOT equal"
 
 
-    
 @app.route("/nth_root", methods=["POST"])
-def nth_root (): 
-    jsonStr  = request.get_json()
-    jsonObj  = json.loads(jsonStr)
-    a1= int(jsonObj['N1'])
-    b1= int(jsonObj['N2'])
-    outpt=((a1)**(1/b1))
-    response  = str(outpt)
-    return  response
+def nth_root():
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    a1 = int(jsonObj['N1'])
+    b1 = int(jsonObj['N2'])
+    outpt = ((a1)**(1/b1))
+    response = str(outpt)
+    return response
     # Logic for function assigned to you as in pdf
-    
-    
+
     # Logic for function assigned to you as in pdf
-    
 
 
 @app.route("/bitAND", methods=["POST"])
-def bitAND(): 
+def bitAND():
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr)
-    a=int(jsonObj['N1'])
-    b=int(jsonObj['N2'])
-    num=a&b
+    a = int(jsonObj['N1'])
+    b = int(jsonObj['N2'])
+    num = a & b
     response = "And operator of 2 numbers is " + str(num)
     return response
 
-@app.route("/logarithm", methods=["POST"])
-def logarithm(): 
-#code added by group tech warriors
-    jsonStr = request.get_json()
-    jsonObj = json.loads(jsonStr)
-    
-    a=int(jsonObj['N1'])
-    b=int(jsonObj['N2'])
 
-    log=math.log(a,b)
-    response="Log of Number-1 is calculated with  Number-2 as the base "+str(log)
-    return response
-@app.route("/multiply", methods=["POST"])
-def multiply(): 
+@app.route("/logarithm", methods=["POST"])
+def logarithm():
+    # code added by group tech warriors
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr)
-    
-    a=int(jsonObj['N1'])
-    b=int(jsonObj['N2'])
-    mul=a*b
+
+    a = int(jsonObj['N1'])
+    b = int(jsonObj['N2'])
+
+    log = math.log(a, b)
+    response = "Log of Number-1 is calculated with  Number-2 as the base " + \
+        str(log)
+    return response
+
+
+@app.route("/multiply", methods=["POST"])
+def multiply():
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+
+    a = int(jsonObj['N1'])
+    b = int(jsonObj['N2'])
+    mul = a*b
     response = f"multiplication of {a} and {b} = {mul}"
     return response
 
+
 @app.route("/LOGICAL_AND", methods=["POST"])
-def LOGICAL_AND(): 
+def LOGICAL_AND():
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr)
-    
-    a=int(jsonObj['N1'])
-    b=int(jsonObj['N2'])
+
+    a = int(jsonObj['N1'])
+    b = int(jsonObj['N2'])
     LOGIC_AND = a and b
     response = "logical and of a and b is = " + str(LOGIC_AND)
 
     return response
-       
+
+
 @app.route("/shiftdeciright", methods=["POST"])
-def shiftdeciright(): 
+def shiftdeciright():
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr)
-    
-    a=float(jsonObj['N1'])
-    b=float(jsonObj['N2'])
-    shift =a*10
-    shift2 =b*10
-    response ="Shfifting decimal right " + str(shift) +" and "+ str(shift2)
+
+    a = float(jsonObj['N1'])
+    b = float(jsonObj['N2'])
+    shift = a*10
+    shift2 = b*10
+    response = "Shfifting decimal right " + str(shift) + " and " + str(shift2)
     return response
-    
+
 
 @app.route("/lor", methods=["POST"])
-def lor(): 
+def lor():
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr)
 
-    a=jsonObj['N1']
-    b=jsonObj['N2']
-    num=a or b
+    a = jsonObj['N1']
+    b = jsonObj['N2']
+    num = a or b
     response = "Logical or of 2 numbers is " + str(num)
     return response
 
+
 @app.route("/xyz", methods=["POST"])
-def XYZ(): 
+def XYZ():
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr)
-    a=int(jsonObj['N1'])
-    b=int(jsonObj['N2'])
-    
+    a = int(jsonObj['N1'])
+    b = int(jsonObj['N2'])
+
     # Logic for function assigned to you as in pdf
     ans = a/(10**b)
     response = "left decimal shift: " + str(ans)
     return response
 
-if __name__== "__main__":
+
+@app.route("/hcf", methods=["POST"])
+def HCF():
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+
+    a = int(jsonObj['N1'])
+    b = int(jsonObj['N2'])
+    sum = gcd(a, b)
+    response = "HCF of 2 numbers = " + str(sum)
+    return response
+
+
+if __name__ == "__main__":
     app.run()
