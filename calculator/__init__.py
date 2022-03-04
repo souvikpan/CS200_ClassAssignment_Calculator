42aae62c207fdcb9f43081e65a8715858e1cbe2d
Riya <78996450+riya107@users.noreply.github.com>
2022-03-04 15:20:02 +0530

    cherry-picked multiply operation by Bit_by_Bit

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -29,5 +29,17 @@ def exp():
 
 
 
+#code added by group tech warriors
+@app.route("/multiply", methods=["POST"])
+def multiply(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    mul=a*b
+    response = f"multiplication of {a} and {b} = {mul}"
+    return response
+
 if __name__== "__main__":
     app.run()
